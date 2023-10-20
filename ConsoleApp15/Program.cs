22333bbb1677015095d306d1a9ce4eb3f021a628
Abdulahi18 <abib05001@utb.vaxjo.se>
2023-10-20 12:56:34 +0200

    Add project files.

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        for (int i = 10; i <= 30; i++)
+        {
+            Console.WriteLine(i);
+        }
+        for (int j = 200; j >= 180; j--)
+        {
+            Console.WriteLine(j);
+        }
+        for (int k = 1000; k <= 1400; k += 50)
+        {
+            Console.WriteLine(k);
+        }
+    }
+}
+      
+
+       
